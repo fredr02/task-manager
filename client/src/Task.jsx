1b5688f8aae32329c47559c7b585e62430f50167
Fred Lopez <me@fredlopez.net>
2023-01-19 17:03:30 -0500

    Hide check if task complete

--- client/src/Task.jsx
+++ client/src/Task.jsx
@@ -6,9 +6,11 @@ const Task = ({ task, index }) => {
   return (
     <div className="h-[100%] flex justify-between items-center min-h-[50px] bg-primary rounded-3xl cursor-pointer p-3">
       <h1 className="text-xl font-light">{task.name}</h1>
-      <button className="bg-[black] bg-opacity-10 rounded-full p-3 hover:bg-opacity-20">
-        {<FiCheck />}
-      </button>
+      {!task.isComplete && (
+        <button className="bg-[black] bg-opacity-10 rounded-full p-3 hover:bg-opacity-20">
+          {<FiCheck />}
+        </button>
+      )}
     </div>
   );
 };
