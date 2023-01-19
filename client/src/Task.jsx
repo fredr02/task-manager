681185606c88c6e8662c9f1b6063ba5f2eb864cf
Fred Lopez <frederickr02@gmail.com>
2023-01-19 06:57:58 -0500

    added filter buttons and basic tasklist items

--- client/src/Task.jsx
+++ client/src/Task.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Task = () => {
-  return <div className="min-h-[50px] bg-primary">Task</div>;
+const Task = ({ task, index }) => {
+  return (
+    <div className="h-[100%] flex justify-between items-center min-h-[50px] bg-primary rounded-3xl p-3">
+      <h1>{task.name}</h1>
+      <button className="bg-[black] bg-opacity-10 rounded-full p-3">✓</button>
+    </div>
+  );
 };
 
 export default Task;
--- client/src/Tasklist.jsx
+++ client/src/Tasklist.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Task from './Task';
 
 const Tasklist = ({ tasks }) => {
+  const [filter, setFilter] = useState('active');
+  let enabled = 'bg-primary text-[black] rounded-full p-2 box-border';
+  let disabled = 'bg-[#0e1621] text-[white] rounded-full p-2 box-border';
+
+  const doneClickHandler = () => setFilter('done');
+  const activeClickHandler = () => setFilter('active');
+
   return (
     <div className="flex flex-col mt-5 gap-4">
-      <div className="flex gap-4">
-        <div className="bg-white rounded-xl font-sm p-3">{tasks.length}</div>
-        <h2 className="text-primary text-5xl font-light">Tasks</h2>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-4">
+          <div className="bg-white rounded-xl font-sm p-3">{tasks.length}</div>
+          <h2 className="text-primary text-5xl font-light">Tasks</h2>
+        </div>
+        <div>
+          <button
+            className={filter == 'active' ? enabled : disabled}
+            onClick={activeClickHandler}
+          >
+            Active
+          </button>
+          <button
+            className={`${filter == 'done' ? enabled : disabled} ml-2`}
+            onClick={doneClickHandler}
+          >
+            Done
+          </button>
+        </div>
       </div>
       <hr className="border-white"></hr>
-      {tasks.map((task) => {
-        <Task task={task} />;
-      })}
+      {tasks.map((task, index) => (
+        <Task key={task._id} task={task} index={index} />
+      ))}
     </div>
   );
 };
