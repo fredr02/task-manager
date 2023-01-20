823f1f6c35298fe5e28d5693268cf9cb59925b27
Fred Lopez <frederickr02@gmail.com>
2023-01-20 05:09:58 -0500

    dim complete tasks

--- client/src/Task.jsx
+++ client/src/Task.jsx
@@ -4,7 +4,11 @@ import { FiCheck } from 'react-icons/fi';
 
 const Task = ({ task, index }) => {
   return (
-    <div className="h-[100%] flex justify-between items-center min-h-[50px] bg-primary rounded-3xl cursor-pointer p-3">
+    <div
+      className={`h-[100%] flex justify-between items-center min-h-[50px] bg-primary rounded-3xl cursor-pointer p-3 ${
+        task.isComplete && 'bg-opacity-30'
+      }`}
+    >
       <h1 className="text-xl font-light">{task.name}</h1>
       {!task.isComplete && (
         <button className="bg-[black] bg-opacity-10 rounded-full p-3 hover:bg-opacity-20">
